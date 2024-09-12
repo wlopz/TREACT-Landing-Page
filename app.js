e41f06e30db8283fc6b1cb0eb649895c53f3b39c
Wilfredo <jrlopez85@gmail.com>
2024-09-12 11:17:17 -0500

    Initial git push to include front end design for treact page. It contains nav bar, header, and other sections with first layout design. Still missing other sections and make it mobile responsive

---
+++ app.js
@@ -0,0 +1,5 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+        
+// Set the current year in the element with id "year"
+document.getElementById("current-year").textContent = currentYear;
