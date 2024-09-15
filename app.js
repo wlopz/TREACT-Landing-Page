77d70efc13fc91c8c0735ef1703640c9c9b3fb55
Wilfredo <jrlopez85@gmail.com>
2024-09-15 03:46:43 -0500

    First round of mobile/tablet responsiveness

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ const currentYear = new Date().getFullYear();
         
 // Set the current year in the element with id "year"
 document.getElementById("current-year").textContent = currentYear;
+
+// MOBILE BURGER MENU
+
+// OPEN
+function openMenu() {
+    document.body.classList += " menu--open"
+}
+
+// CLOSE
+function closeMenu() {
+    document.body.classList.remove('menu--open')
+}
